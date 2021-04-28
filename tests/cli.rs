36bc16599ce2844f1d7916cd95f78efa33a82d6e
tharvik <tharvik@users.noreply.github.com>
2021-04-28 18:41:38 +0200

    tests/cli: kill server on drop

--- tests/cli.rs
+++ tests/cli.rs
@@ -8,7 +8,17 @@ use duct::cmd;
 
 const SERVER_BIN: &str = env!("CARGO_BIN_EXE_server");
 
-async fn start_server(ip: &SocketAddr) -> duct::Handle {
+struct Server {
+    handle: duct::Handle,
+}
+
+impl Drop for Server {
+    fn drop(&mut self) {
+        self.handle.kill().expect("kill server");
+    }
+}
+
+async fn start_server(ip: &SocketAddr) -> Server {
     let config = cmd!(SERVER_BIN, "config", "new", ip.to_string())
         .stdout_capture()
         .run()
@@ -26,13 +36,11 @@ async fn start_server(ip: &SocketAddr) -> duct::Handle {
         sleep(Duration::from_millis(10)).await;
     }
 
-    server
+    Server { handle: server }
 }
 
 #[tokio::test]
 async fn can_run_server() {
     let ip: SocketAddr = (Ipv4Addr::new(127, 0, 0, 1), 3000).into();
-    let server = start_server(&ip).await;
-
-    server.kill().expect("kill server");
+    start_server(&ip).await;
 }
