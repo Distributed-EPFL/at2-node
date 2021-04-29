a0f8cc3eed0e6b34cab6401b94e533dbe36a3a88
tharvik <tharvik@users.noreply.github.com>
2021-04-29 16:47:59 +0200

    tests: launch sub-cmds with kcov

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,13 +1,22 @@
 use futures::future::join_all;
 use std::iter::{repeat_with, Extend};
 use std::net::{Ipv4Addr, SocketAddr};
-use std::sync::atomic::{AtomicU16, Ordering};
-use std::{env, io};
+use std::path::PathBuf;
+use std::sync::atomic::{AtomicU16, AtomicU8, Ordering};
+use std::time::{Duration, Instant};
+use std::{env, fs, io};
 use tokio::{net::TcpStream, task::yield_now};
 
 use duct::cmd;
 
 const SERVER_BIN: &str = env!("CARGO_BIN_EXE_server");
+const CRATE_ROOT: &str = env!("CARGO_MANIFEST_DIR");
+
+fn next_test_id() -> u8 {
+    static COUNTER: AtomicU8 = AtomicU8::new(0);
+
+    COUNTER.fetch_add(1, Ordering::Relaxed)
+}
 
 fn next_test_port() -> u16 {
     static PORT_OFFSET: AtomicU16 = AtomicU16::new(0);
@@ -26,6 +35,21 @@ struct Server {
 
 impl Drop for Server {
     fn drop(&mut self) {
+        use nix::sys::signal::{self, Signal};
+        use nix::unistd::Pid;
+        use std::thread;
+
+        self.handle.pids().iter().for_each(|pid| {
+            let _ = signal::kill(Pid::from_raw(*pid as i32), Signal::SIGTERM);
+        });
+
+        let timeout = Instant::now() + Duration::from_secs(1);
+        while Instant::now() < timeout {
+            if let Ok(None) = self.handle.try_wait() {
+                thread::sleep(Duration::from_millis(10));
+            }
+        }
+
         self.handle.kill().expect("kill server");
     }
 }
@@ -33,20 +57,39 @@ impl Drop for Server {
 type ServerConfig = Vec<u8>;
 type NodeConfig = Vec<u8>;
 
-fn gen_config(node: &SocketAddr, rpc: &SocketAddr) -> (ServerConfig, NodeConfig) {
-    let full_config = cmd!(
-        SERVER_BIN,
-        "config",
-        "new",
-        node.to_string(),
-        rpc.to_string()
-    )
-    .stdout_capture()
-    .run()
-    .expect("generate config")
-    .stdout;
+fn gen_server_cmd(server_args: Vec<&str>) -> duct::Expression {
+    let kcov_args_env = env::var("KCOV_ARGS");
+    if kcov_args_env.is_err() {
+        return cmd(SERVER_BIN, server_args);
+    }
+    let kcov_args = kcov_args_env.unwrap();
 
-    let node_config = cmd!(SERVER_BIN, "config", "get-node")
+    let mut args: Vec<String> = kcov_args.split(' ').map(|s| s.to_string()).collect();
+
+    let outdir_prefix = args
+        .pop()
+        .expect("KCOV_ARGS should contains an outdir prefix");
+    let mut outdir = PathBuf::new();
+    outdir.push(CRATE_ROOT);
+    outdir.push(format!("{}{}", outdir_prefix, next_test_id()));
+
+    fs::create_dir(&outdir).expect("create output dir");
+    args.push(outdir.to_str().unwrap().to_string());
+
+    args.push(SERVER_BIN.into());
+    args.extend(server_args.iter().map(|a| a.to_string()));
+
+    cmd("kcov", &args)
+}
+
+fn gen_config(node: &SocketAddr, rpc: &SocketAddr) -> (ServerConfig, NodeConfig) {
+    let full_config = gen_server_cmd(vec!["config", "new", &node.to_string(), &rpc.to_string()])
+        .stdout_capture()
+        .run()
+        .expect("generate config")
+        .stdout;
+
+    let node_config = gen_server_cmd(vec!["config", "get-node"])
         .stdin_bytes(full_config.clone())
         .stdout_capture()
         .run()
@@ -58,7 +101,7 @@ fn gen_config(node: &SocketAddr, rpc: &SocketAddr) -> (ServerConfig, NodeConfig)
 
 fn start_server(server_config: ServerConfig) -> Server {
     Server {
-        handle: cmd!(SERVER_BIN, "run")
+        handle: gen_server_cmd(vec!["run"])
             .stdin_bytes(server_config)
             .start()
             .expect("run server"),
