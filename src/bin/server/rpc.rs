1f6c7e1559eb53f6839fec5055058125669ca447
tharvik <tharvik@users.noreply.github.com>
2021-05-06 18:29:48 +0200

    bin/server: add network support

--- src/bin/server/rpc.rs
+++ src/bin/server/rpc.rs
@@ -1,3 +1,15 @@
+use std::net::SocketAddr;
+
+use drop::crypto::key::exchange::{self, Exchanger};
+use drop::crypto::sign;
+use drop::net::{ConnectorExt, TcpConnector, TcpListener};
+use drop::system::manager::{Handle, SystemManager};
+use drop::system::sampler::AllSampler;
+use drop::system::sender::NetworkSender;
+use drop::system::System;
+use murmur::MurmurConfig;
+use sieve::{self, Sieve, SieveConfig, SieveMessage};
+
 use tonic::Response;
 
 tonic::include_proto!("at2");
@@ -5,8 +17,60 @@ tonic::include_proto!("at2");
 use at2_server::At2;
 pub use at2_server::At2Server;
 
-#[derive(Default)]
-pub struct Service {}
+type M = u64;
+
+pub struct Service {
+    handle: sieve::SieveHandle<M, NetworkSender<SieveMessage<M>>, sieve::Fixed>,
+}
+
+use super::config;
+
+impl Service {
+    pub async fn new(
+        listener_addr: SocketAddr,
+        network_keypair: exchange::KeyPair,
+        sign_keypair: sign::KeyPair,
+        network: Vec<config::Node>,
+    ) -> Self {
+        let exchanger = Exchanger::new(network_keypair);
+
+        let listener = TcpListener::new(listener_addr, exchanger.clone())
+            .await
+            .expect("failed to listen for incoming connections");
+
+        let (addrs, keys): (Vec<_>, Vec<_>) = network
+            .iter()
+            .map(|node| (node.address, node.public_key))
+            .unzip();
+        let connector = TcpConnector::new(exchanger).retry();
+        let mut system = System::new_with_connector(&connector, keys, addrs).await;
+
+        let _ = system.add_listener(listener).await;
+        let manager = SystemManager::new(system);
+
+        let sieve = Sieve::new(
+            sign_keypair,
+            sieve::Fixed::new_local(),
+            SieveConfig {
+                sieve_sample_size: 1,
+                murmur: MurmurConfig {
+                    murmur_gossip_size: network.len(),
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+        );
+
+        let sampler = AllSampler::default();
+
+        Self {
+            handle: manager
+                .run(sieve, sampler, num_cpus::get())
+                .await
+                .processor_handle(),
+        }
+    }
+}
 
 #[tonic::async_trait]
 impl At2 for Service {
@@ -14,6 +78,19 @@ impl At2 for Service {
         &self,
         _request: tonic::Request<SendMoneyRequest>,
     ) -> Result<tonic::Response<SendMoneyReply>, tonic::Status> {
+        // TODO by the client
+        let sign_keypair = sign::KeyPair::random();
+        let mut signer = sign::Signer::new(sign_keypair.clone());
+        let message: u64 = 0;
+        let signature = signer.sign(&message).expect("sign failed");
+        let payload = sieve::Payload::new(*sign_keypair.public(), 0, message, signature);
+
+        self.handle
+            .clone()
+            .broadcast(&payload)
+            .await
+            .expect("broadcasting failed");
+
         let reply = SendMoneyReply { request_id: vec![] };
         Ok(Response::new(reply))
     }
--- tests/cli.rs
+++ tests/cli.rs
@@ -1,12 +1,24 @@
-use std::env;
-use std::io;
+use futures::future::join_all;
+use std::iter::{repeat_with, Extend};
 use std::net::{Ipv4Addr, SocketAddr};
-use std::time::Duration;
-use tokio::{net::TcpStream, time::sleep};
+use std::sync::atomic::{AtomicU16, Ordering};
+use std::{env, io};
+use tokio::{net::TcpStream, task::yield_now};
 
 use duct::cmd;
 
 const SERVER_BIN: &str = env!("CARGO_BIN_EXE_server");
+
+fn next_test_port() -> u16 {
+    static PORT_OFFSET: AtomicU16 = AtomicU16::new(0);
+    const PORT_START: u16 = 3000;
+
+    PORT_START + PORT_OFFSET.fetch_add(1, Ordering::Relaxed)
+}
+
+fn next_test_ip4() -> SocketAddr {
+    (Ipv4Addr::new(127, 0, 0, 1), next_test_port()).into()
+}
 
 struct Server {
     handle: duct::Handle,
@@ -18,29 +30,115 @@ impl Drop for Server {
     }
 }
 
-async fn start_server(ip: &SocketAddr) -> Server {
-    let config = cmd!(SERVER_BIN, "config", "new", ip.to_string())
+type ServerConfig = Vec<u8>;
+type NodeConfig = Vec<u8>;
+
+fn gen_config(node: &SocketAddr, rpc: &SocketAddr) -> (ServerConfig, NodeConfig) {
+    let full_config = cmd!(
+        SERVER_BIN,
+        "config",
+        "new",
+        node.to_string(),
+        rpc.to_string()
+    )
+    .stdout_capture()
+    .run()
+    .expect("generate config")
+    .stdout;
+
+    let node_config = cmd!(SERVER_BIN, "config", "get-node")
+        .stdin_bytes(full_config.clone())
         .stdout_capture()
         .run()
-        .expect("generate config");
+        .expect("get node config")
+        .stdout;
 
-    let server = cmd!(SERVER_BIN, "run")
-        .stdin_bytes(config.stdout)
-        .start()
-        .expect("run server");
+    (full_config, node_config)
+}
 
-    while let Err(err) = TcpStream::connect(ip).await {
+fn start_server(server_config: ServerConfig) -> Server {
+    Server {
+        handle: cmd!(SERVER_BIN, "run")
+            .stdin_bytes(server_config)
+            .start()
+            .expect("run server"),
+    }
+}
+
+async fn wait_until_connect(server: &Server, to_probe: &SocketAddr) {
+    while let Err(err) = TcpStream::connect(to_probe).await {
         if err.kind() != io::ErrorKind::ConnectionRefused {
             panic!("connect server: {}", err)
         }
-        sleep(Duration::from_millis(10)).await;
-    }
 
-    Server { handle: server }
+        if let Err(err) = server.handle.try_wait() {
+            server
+                .handle
+                .wait()
+                .unwrap_or_else(|_| panic!("server finished early: {}", err));
+        }
+
+        yield_now().await;
+    }
 }
 
 #[tokio::test]
-async fn can_run_server() {
-    let ip: SocketAddr = (Ipv4Addr::new(127, 0, 0, 1), 3000).into();
-    start_server(&ip).await;
+async fn server_without_network_fails() {
+    let (node, rpc) = (next_test_ip4(), next_test_ip4());
+
+    let (server_config, _) = gen_config(&node, &rpc);
+
+    let server = start_server(server_config);
+    let exit = server.handle.wait();
+    assert_eq!(exit.err().map(|err| err.kind()), Some(io::ErrorKind::Other))
+}
+
+async fn start_network(size: usize) -> SocketAddr {
+    let addresses = repeat_with(|| (next_test_ip4(), next_test_ip4()))
+        .take(size)
+        .collect::<Vec<_>>();
+
+    let (mut server_configs, node_configs): (Vec<_>, Vec<_>) = addresses
+        .iter()
+        .map(|(node, rpc)| gen_config(&node, &rpc))
+        .unzip();
+
+    server_configs
+        .iter_mut()
+        .enumerate()
+        .for_each(|(server_pos, server_config)| {
+            server_config.extend(
+                node_configs
+                    .iter()
+                    .enumerate()
+                    .filter(|(node_pos, _)| *node_pos != server_pos)
+                    .map(|(_, node_config)| node_config)
+                    .flatten(),
+            )
+        });
+
+    let servers: Vec<_> = server_configs
+        .iter()
+        .map(|server_config| start_server(server_config.clone()))
+        .collect();
+
+    join_all(servers.iter().zip(&addresses).flat_map(|(server, addrs)| {
+        vec![
+            wait_until_connect(&server, &addrs.0),
+            wait_until_connect(&server, &addrs.1),
+        ]
+    }))
+    .await;
+
+    addresses
+        .iter()
+        .map(|(_, rpc)| rpc)
+        .copied()
+        .next()
+        .expect("zero sized network")
+}
+
+#[tokio::test]
+async fn can_run_network() {
+    start_network(3).await;
 }
