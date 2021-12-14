d4782532faaf3b0d4e1ca98f083ec24b800e226b
tharvik <tharvik@users.noreply.github.com>
2021-12-14 16:10:39 +0100

    server/accounts: inc seq even if failing

--- src/bin/server/accounts/account.rs
+++ src/bin/server/accounts/account.rs
@@ -7,8 +7,8 @@ pub enum Error {
     Underflow,
 }
 
-/// Contains the balance for a user
-#[derive(Debug)]
+/// Contains the balance and the latest processed sequence for a user
+#[derive(Debug, Clone, Copy)]
 pub struct Account {
     last_sequence: sieve::Sequence,
     balance: u64,
@@ -26,21 +26,20 @@ impl Account {
     }
 
     /// Add some amount to this account
-    pub fn credit(&self, amount: u64) -> Result<Self, Error> {
-        Ok(Self {
-            last_sequence: self.last_sequence,
-            balance: self.balance.checked_add(amount).context(Overflow)?,
-        })
+    pub fn credit(&mut self, amount: u64) -> Result<(), Error> {
+        self.balance = self.balance.checked_add(amount).context(Overflow)?;
+
+        Ok(())
     }
 
     /// Remove some amount from this account, iff the `sequence` is consecutive to the last one
-    pub fn debit(&self, sequence: sieve::Sequence, amount: u64) -> Result<Self, Error> {
+    pub fn debit(&mut self, sequence: sieve::Sequence, amount: u64) -> Result<(), Error> {
         ensure!(self.last_sequence + 1 == sequence, InconsecutiveSequence);
+        self.last_sequence = sequence;
 
-        Ok(Self {
-            last_sequence: sequence,
-            balance: self.balance.checked_sub(amount).context(Underflow)?,
-        })
+        self.balance = self.balance.checked_sub(amount).context(Underflow)?;
+
+        Ok(())
     }
 
     /// Return the last used sequence
@@ -60,28 +59,33 @@ mod tests {
 
     #[test]
     fn debit_too_much_fails() {
-        let account = Account::new();
+        let mut account = Account::new();
 
+        let old_seq = account.last_sequence();
         account
             .debit(1, INITIAL_BALANCE + 1)
             .expect_err("able to debit more than possessed");
+
+        assert!(old_seq < account.last_sequence());
     }
 
     #[test]
     fn debit_increase_sequence() {
-        let account = Account::new();
+        let mut account = Account::new();
 
-        let new_accout = account.debit(1, 1).expect("to debit account");
+        let old_seq = account.last_sequence();
+        account.debit(1, 1).expect("to debit account");
 
-        assert!(account.last_sequence() < new_accout.last_sequence());
+        assert!(old_seq < account.last_sequence());
     }
 
     #[test]
     fn credit_doesnt_change_sequence() {
-        let account = Account::new();
+        let mut account = Account::new();
 
-        let new_accout = account.credit(1).expect("to credit account");
+        let old_seq = account.last_sequence();
+        account.credit(1).expect("to credit account");
 
-        assert_eq!(account.last_sequence(), new_accout.last_sequence());
+        assert_eq!(old_seq, account.last_sequence());
     }
 }
